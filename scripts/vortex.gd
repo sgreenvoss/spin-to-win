e1fadae469fa58b17595a0fd00de75b06edd9bf2
sgreenvoss <sgreenvoss@gmail.com>
2026-06-20 18:23:23 -0600

    vortex stays in one place, has initial bad graphics for vortexing/out

--- scripts/vortex.gd
+++ scripts/vortex.gd
@@ -2,6 +2,7 @@ extends Node2D
 var mousepos
 @onready var area_2d = $Area2D
 @onready var collision_shape_2d = $Area2D/CollisionShape2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
 var rotation_speed = 0.05
 var vortex_duration = 1.0
 var vortex_delay = 1.0
@@ -10,17 +11,20 @@ var vortexing = false
 var in_vortex = 0.0 # counts the time spent in the vortex
 
 func _process(delta):
-	mousepos = get_global_mouse_position()
-	position = mousepos
-	
 	if vortexing:
 		vortex(delta)
 	else:
+		mousepos = get_global_mouse_position()
+		position = mousepos
 		time_since_v += delta
-		if time_since_v > vortex_delay and Input.is_action_pressed("vortex"):
-			in_vortex = 0
-			vortexing = true
-			vortex(delta)
+		if time_since_v > vortex_delay:
+			animated_sprite_2d.play("default")
+			
+			if Input.is_action_pressed("vortex"):
+				animated_sprite_2d.play("vortex")
+				in_vortex = 0
+				vortexing = true
+				vortex(delta)
 
 func vortex(delta):
 	in_vortex += delta
@@ -34,6 +38,7 @@ func vortex(delta):
 			body.rotation += rotation_speed
 			
 	if in_vortex > vortex_duration or Input.is_action_just_released("vortex"):
+		animated_sprite_2d.play("out")
 		vortexing = false
 		time_since_v = 0
 		for body in objs:
